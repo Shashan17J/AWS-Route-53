58cf54b05c1be02cfeeef80b3c0d2f9cfb53031f
Shashank <shashank.jangid17@gmail.com>
2024-05-09 08:17:27 +0530

    Change BASE_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:4000/api/v1";
+// const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = "https://lucidgrowthbackend.onrender.com/api/v1";
 
 export const route53Endpoints = {
   CREATE_DNS_RECORD: BASE_URL + "/route53/dns",
